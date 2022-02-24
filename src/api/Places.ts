29a6599f4847c4b63c3d524dfc526ae3d2e1c1b4
Alex Maslovsky <alexey.maslovsky@satelliteinnovations.io>
2022-02-24 20:34:52 +0300

    inital

---
+++ src/api/Places.ts
@@ -0,0 +1,53 @@
+export enum PlaceType {
+  Basement = 'basement',
+  Metro = 'metro',
+  Bunker = 'bunker',
+}
+
+export interface IPlace {
+  id: string;
+  address: string;
+  description: string;
+  capacity: number;
+  imageSrc: string;
+  createdAt: Date;
+  type: PlaceType;
+}
+
+const FAKE_DATA: IPlace[] = [
+  {
+    id: 'id1',
+    address: 'Street 1',
+    description: '',
+    capacity: 10,
+    imageSrc: '',
+    createdAt: new Date(),
+    type: PlaceType.Basement,
+  },
+  {
+    id: 'id2',
+    address: 'Street 123',
+    description: '',
+    capacity: 3,
+    imageSrc: '',
+    createdAt: new Date(),
+    type: PlaceType.Bunker,
+  },
+  {
+    id: 'id3',
+    address: 'Street 234',
+    description: '',
+    capacity: 500,
+    imageSrc: '',
+    createdAt: new Date(),
+    type: PlaceType.Metro,
+  },
+];
+
+export default class PlacesApi {
+  public getAll(): Promise<IPlace[]> {
+    return new Promise((resolve) => {
+      setTimeout(() => resolve(FAKE_DATA), 1000);
+    });
+  }
+}
---
+++ src/pages/Home/Home.tsx
@@ -0,0 +1,11 @@
+import { FC } from 'react';
+
+const Home: FC = () => {
+  return (
+    <div>
+      Hello World
+    </div>
+  );
+};
+
+export default Home;
